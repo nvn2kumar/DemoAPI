439d4827bc4c796b08bcf83c286999220418bbab
Naveen Kumar <nvnkumar132@gmail.com>
2026-04-23 02:16:12 +0530

    New Method added in Account and Weather controller

--- DemoAPI/Controllers/WeatherForecastController.cs
+++ DemoAPI/Controllers/WeatherForecastController.cs
@@ -35,5 +35,17 @@ namespace DemoAPI.Controllers
             })
             .ToArray();
         }
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Test()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                //TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
