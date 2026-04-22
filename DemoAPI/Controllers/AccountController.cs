5bea4a6ebf3e4b7409181de7decbcbf8fe15bdab
Naveen Kumar <nvnkumar132@gmail.com>
2026-04-23 02:33:33 +0530

    Remove Test method and update WeatherForecast properties
    The `Test` method has been removed from both `AccountController.cs` and `WeatherForecastController.cs`. In `WeatherForecastController.cs`, the method was updated to include the `TemperatureC` property in the `WeatherForecast` object, which is now populated along with the `Date` and `Summary` properties.

--- DemoAPI/Controllers/AccountController.cs
+++ DemoAPI/Controllers/AccountController.cs
@@ -103,18 +103,5 @@ namespace DemoAPI.Controllers
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Test()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                //TemperatureC = rng.Next(-20, 55),
-                //Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
